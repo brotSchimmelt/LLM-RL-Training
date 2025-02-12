ddec092f6b826a5ed5357b2f8e5d43ce9139cb55
brotSchimmelt <tknebler+git@googlemail.com>
2025-02-12 12:51:21 +0100

    feat: add script to download all models

---
+++ download_llms.py
@@ -0,0 +1,33 @@
+import os
+
+from huggingface_hub import snapshot_download
+
+from src.config import DEFAULT_SETTINGS, MODEL_PATHS
+
+SAVE_DIR = DEFAULT_SETTINGS["model_save_dir"]
+
+
+def download_model(model_name: str, model_save_path: str) -> None:
+    """
+    Downloads a given LLM model from Hugging Face and stores it in the specified directory.
+
+    Args:
+        model_name (str): The name of the model to download from Hugging Face.
+        model_save_path (str): The directory where the model should be stored.
+    """
+    if not os.path.exists(model_save_path):
+        print(f"Downloading {model_name} from Hugging Face...")
+        snapshot_download(repo_id=model_name, local_dir=model_save_path)
+        print(f"Model saved at: {model_save_path}")
+    else:
+        print(f"Model already exists at: {model_save_path}, skipping download.")
+
+
+if __name__ == "__main__":
+    os.makedirs(SAVE_DIR, exist_ok=True)
+
+    # download LLM
+    download_model(DEFAULT_SETTINGS["llm"], MODEL_PATHS["llm"])
+
+    # download LLM judge
+    download_model(DEFAULT_SETTINGS["judge_model"], MODEL_PATHS["judge"])
