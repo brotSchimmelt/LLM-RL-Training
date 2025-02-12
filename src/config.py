eb4a057a96c5c67548f5e4b8ba30af29190b1f8a
brotSchimmelt <tknebler+git@googlemail.com>
2025-02-12 12:27:18 +0100

    chore: add new default llms

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 DEFAULT_SETTINGS = {
-    "judge_model": "Qwen/Qwen2.5-3B-Instruct",
+    "judge_model": "Qwen/Qwen2.5-7B-Instruct",
     "judge_max_model_length": 100,
+    "llm": "unsloth/Llama-3.2-1B",
 }
