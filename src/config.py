b3c3780c9b33f9e60879f352797f5fa3fa49f6c7
brotSchimmelt <tknebler+git@googlemail.com>
2025-02-12 14:16:55 +0100

    refactor: add max context length for llms

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 DEFAULT_SETTINGS = {
     "judge_model": "Qwen/Qwen2.5-7B-Instruct",
-    "judge_max_model_length": 100,
+    "judge_max_model_length": 8_192,
     "llm": "unsloth/Llama-3.2-1B",
+    "llm_max_length": 4_096,
     "model_save_dir": "./models/",
 }
 
